0620738d302e86e89f1b1530f279b6a98b8ee222
Joey35233 <joey35233@gmail.com>
2019-05-25 22:16:46 -0400

    Moving to other device. Do not build.

---
+++ PInvoke.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "PInvoke.h"
+
+void GetFileNames(uint datPathCount, wchar** datPaths, uint dictionaryLineCount, wchar** dictionaryEntries, uint* fileNameCount, wchar** fileNames)
+{
+	auto SQARs = new Fs::SQAR::SQAR[datPathCount];
+}
--- PInvoke.h
+++ PInvoke.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "pch.h"
+#include "SQAR.h"
+#include "Hashing.h"
+
+extern "C"
+{
+	void GetFileNames(uint datPathCount, wchar** datPaths, uint dictionaryLineCount, wchar** dictionaryEntries, uint* fileNameCount, wchar** fileNames);
+}
